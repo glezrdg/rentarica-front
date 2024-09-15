13333c8e7f048fef88be76be77302fd94cd01a86
Carlos Bueno <developer.carlosbueno@gmail.com>
2024-09-15 13:09:27 -0400

    mobile nav

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,9 +1,11 @@
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
 
 const Header = () => {
+  const [mobileNav, setMobileNav] = useState(false)
+
   return (
-    <header className='h-[85px] md:h-[100px]'>
+    <header className='fixed bg-white w-[100vw] z-50 h-[85px] md:h-[100px]'>
       <nav className='flex items-center justify-between h-full w-full container m-auto px-4 xl:px-10'>
         {/* LOGO */}
         <Link href={'/'}>
@@ -12,6 +14,11 @@ const Header = () => {
             src='/assets/logos/logo_light.png'
           />
         </Link>
+
+        <i
+          onClick={() => setMobileNav(!mobileNav)}
+          className='pi pi-align-right text-2xl text-slate-500 hover:text-orange-600'
+        />
 
         {/* NAVIGATION LINKS */}
         <ul className='items-center gap-5 lg:gap-8 hidden md:flex'>
@@ -22,7 +29,7 @@ const Header = () => {
             Servicios
           </Link>
           <Link href={'/#projects'} className='cursor-pointer'>
-            Proyectos
+            Proyecto bg-orange-600 -right-fulls
           </Link>
           <Link href={'/#clients'} className='cursor-pointer'>
             Clientes
@@ -32,6 +39,50 @@ const Header = () => {
           </Link>
         </ul>
       </nav>
+
+      <div
+        className={`fixed w-[100vw] h-[100vh] bg-orange-600 transition-all ${
+          mobileNav ? 'right-0' : '!right-full'
+        } `}
+      >
+        <ul className='grid h-[60%] place-items-center text-white gap-0 lg:gap-8'>
+          <Link
+            onClick={() => setMobileNav(false)}
+            href={'/#perfil'}
+            className='cursor-pointer'
+          >
+            Perfil
+          </Link>
+          <Link
+            onClick={() => setMobileNav(false)}
+            href={'/#services'}
+            className='cursor-pointer'
+          >
+            Servicios
+          </Link>
+          <Link
+            onClick={() => setMobileNav(false)}
+            href={'/#projects'}
+            className='cursor-pointer'
+          >
+            Proyecto
+          </Link>
+          <Link
+            onClick={() => setMobileNav(false)}
+            href={'/#clients'}
+            className='cursor-pointer'
+          >
+            Clientes
+          </Link>
+          <Link
+            onClick={() => setMobileNav(false)}
+            href={'/#contact'}
+            className='btn-orange !bg-white !text-orange-600 !px-3 !py-3'
+          >
+            Contactanos
+          </Link>
+        </ul>
+      </div>
     </header>
   )
 }
