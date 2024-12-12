3fccfcedfb4f541804634e1a7f61f21a00bbde4f
Carlos Bueno <Developer.carlosbueno@gmail.com>
2024-12-12 17:15:49 -0400

    Invertir page mockup  done

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -47,3 +47,32 @@ export const propertyFeatures = [
     icon: "/assets/icons/unfurnished.svg",
   },
 ];
+
+export const upcomingFairs = [
+  {
+    id: 1,
+    title: 'New York | Lawrence',
+    date: '7 y 8 de Diciembre',
+    time: '10am a 8PM',
+    description: 'Pendiente definir ubicación. Inscríbete si te interesa y estaremos enviando la información una vez tengamos todo confirmado.',
+    img: 'https://static.wixstatic.com/media/828c94_4201a7f3fac541c8b9b22322e034c1fc~mv2.jpg/v1/fill/w_345,h_464,al_c,q_80,usm_0.66_1.00_0.01,enc_avif,quality_auto/Pier-6-at-Brooklyn-Bridge-Park.jpg'
+  },
+  {
+    id: 2,
+    title: 'España, Madrid',
+    description: 'Regístrate si deseas asistir y estaremos informándote cuando obtengamos fecha y ubicación del evento',
+    img: 'https://static.wixstatic.com/media/828c94_a0a4e1e40db440828febafd1fd293047~mv2.webp/v1/fill/w_345,h_464,al_c,q_80,usm_0.66_1.00_0.01,enc_avif,quality_auto/Espa%C3%B1a%20Madrid.webp'
+  },
+  {
+    id: 3,
+    title: 'Puerto Rico',
+    description: 'Regístrate si deseas asistir y estaremos informándote cuando obtengamos fecha y ubicación del evento',
+    img: 'https://static.wixstatic.com/media/828c94_b25a74c6e1fa401c95af652205a1f9a4~mv2.jpg/v1/fill/w_345,h_464,al_c,q_80,usm_0.66_1.00_0.01,enc_avif,quality_auto/Puerto%20Rico.jpg'
+  },
+  {
+    id: 4,
+    title: 'Canada',
+    description: 'Regístrate si deseas asistir y estaremos informándote cuando obtengamos fecha y ubicación del evento',
+    img: 'https://static.wixstatic.com/media/828c94_d0b75c3c11254ceba4975a04bb0d7a9b~mv2.jpeg/v1/crop/x_25,y_0,w_163,h_186/fill/w_194,h_260,al_c,lg_1,q_80,enc_avif,quality_auto/Canada.jpeg'
+  },
+]
